e1621daa4797cb7b328cc119634bc99dd4f8bdae
Sebastian Kowalski <wwy.skowalski@wp.pl>
2022-12-19 19:01:24 +0100

    Add module 9.3

--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -145,7 +145,7 @@ class Product{
     const event = new CustomEvent('add-to-cart', {
       bubbles: true,
       detail: {
-        product: thisProduct,
+        product: thisProduct.prepareCartProduct(),
       },
     });
 
