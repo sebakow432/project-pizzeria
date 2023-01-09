c50e8f88ed9d257ca110a3b55f0daa430f783cc7
Sebastian Kowalski <wwy.skowalski@wp.pl>
2023-01-09 11:40:24 +0100

    Add module 9.4

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,63 @@
 import {settings, select, templates, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisAppp.activePage whit that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active to matching pages, remove from non-matching" */
+    for(let page of thisApp.pages){
+      page.classList.toggle(
+        classNames.pages.active, 
+        page.id == pageId
+      );
+    }
+    /* add class "active to matching links, remove from non-matching" */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function(){
     for(let productData in this.data.products){
       new Product(this.data.products[productData].id, this.data.products[productData]);
@@ -19,6 +74,12 @@ const app = {
     thisApp.productList.addEventListener('add-to-cart', function(event){
       app.cart.add(event.detail.product);
     });
+  },
+
+  initBooking: function () {
+    const thisApp = this;
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingContainer);
   },
 
   initData: function () {
@@ -45,8 +106,10 @@ const app = {
     console.log('settings:', settings);
     console.log('templates:', templates);
 
+    this.initPages();
     this.initData();
     this.initCart();
+    this.initBooking();
   },
 };
 
